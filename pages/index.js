36166d6e09f3078d990492d3c5a2687c78c7cdcb
Kabaloutz <80052137+Kabaloutz@users.noreply.github.com>
2021-03-04 11:31:22 -0400

    mudando

--- pages/index.js
+++ pages/index.js
@@ -5,13 +5,13 @@ export default function Home() {
   return (
     <div className={styles.container}>
       <Head>
-        <title>Create Next App</title>
+        <title>jean</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-         jean <a href="https://nextjs.org">Next.js!</a>
+         jean kabal
         </h1>
 
         <p className={styles.description}>
