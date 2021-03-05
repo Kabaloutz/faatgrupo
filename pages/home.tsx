6666a3a192a175b64c52f6265e935d5d2003c141
Kabaloutz <80052137+Kabaloutz@users.noreply.github.com>
2021-03-05 15:12:05 -0400

    .

--- pages/home.tsx
+++ pages/home.tsx
@@ -1,9 +1,5 @@
 import {
-    Heading, Grid, GridItem, Flex, Spacer, Box, Button,
-    Breadcrumb,
-    BreadcrumbItem,
-    BreadcrumbLink,
-    BreadcrumbSeparator,
+    Heading, Grid, GridItem, Flex, Spacer, Box, Button, Link,
 } from "@chakra-ui/react"
 import theme from '../styles/theme';
 
@@ -12,7 +8,7 @@ export default function Home() {
         <Grid
             as="main"
             height="100vh"
-            templateColluns="1fr 1fr 1fr"
+            templateColumns="1fr 1080px 1fr"
             templateRows="200px 50px 1fr "
             templateAreas="
             '. cabecalho .'
@@ -25,42 +21,35 @@ export default function Home() {
                 <Box w="100%" h="100%" bg="brand.100">  </Box>
             </Flex>
             {/* Cabeçalho */}
-            <Flex gridArea="cabecalho" bg="brand.100" center>
-
+            <Flex gridArea="cabecalho" bg="brand.100" flexDir="row">
                 <Box p="20"><img src="/faatlogo.png" alt="logo do grupo faat" /> </Box>
-                <Spacer />
                 <Box p="20"><img src="/slogan.png" alt="Nosso Slogan: A escolha certa para sua obra" /></Box>
-                <Spacer />
                 <Box p="10"><img src="/mascote.png" alt="Tijolito mascote do grupo" /></Box>
             </Flex>
-            <Flex gridArea="menu">
-                <Box bg="yellow.700" w="100%" >
-                    <Breadcrumb  >
-                        <BreadcrumbItem>
-                            <BreadcrumbLink href="#">Home</BreadcrumbLink>
-                        </BreadcrumbItem>
-                        <BreadcrumbItem>
-                            <BreadcrumbLink href="#">EMPRESA</BreadcrumbLink>
-                        </BreadcrumbItem>
-                        <BreadcrumbItem isCurrentPage>
-                            <BreadcrumbLink href="#">PRODUTOS</BreadcrumbLink>
-                        </BreadcrumbItem>
-                    </Breadcrumb>
-                </Box>
+            <Flex gridArea="menu" bg="yellow.700" mr={25px} alignItems="center" justifyContent="center" flexDir="row" >
+                <Box  ><Link>HOME</Link></Box>
+                <Spacer />
+                <Box  ><Link>EMPRESA</Link></Box>
+                <Spacer />
+                <Box ><Link>PRODUTOS</Link></Box>
+                <Spacer />
+                <Box ><Link>SIMULADOR</Link></Box>
+                <Spacer />
+                <Box ><Link>CONTATOS</Link></Box>
             </Flex>
-            <Flex gridArea="principal">
-                <Box bg="yellow.900" w="100%">espaço produtos</Box>
-                <Box bg="yellow.700" >
+            <Flex gridArea="principal" bg="yellow.900">
+                <Box  w="100%">espaço produtos</Box>
+                <Box  >
                     espaço barra lateral
-                    <Box p="3" bg="yellow.900" >banner</Box>
+                    <Box p="3" bg="yellow.700" w="200px" h="200px">banner</Box>
+                    
+                    <Box p="3" bg="yellow.700" w="200px" h="200px">banner</Box>
                     <Spacer />
-                    <Box p="3" bg="yellow.700" >banner</Box>
-                    <Spacer />
-                    <Box p="3" bg="yellow.900" >banner</Box>
+                    <Box p="3" bg="yellow.700" w="200px" h="200px">banner</Box>
                 </Box>
             </Flex>
             <Flex gridArea="esquerda">
-                <Box w="100%" h="100%" bg="brand.100">f </Box>
+           
             </Flex>
         </Grid>
 
