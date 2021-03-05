4478d38993dbea2e9934cc9d62b709e870b3855a
Kabaloutz <80052137+Kabaloutz@users.noreply.github.com>
2021-03-05 09:24:16 -0400

    iniciando com grid

--- pages/home.tsx
+++ pages/home.tsx
@@ -1,10 +1,24 @@
-import {  Heading, Grid, GridItem } from "@chakra-ui/react"
+import {  Heading, Grid, GridItem Flex, Spacer} from "@chakra-ui/react"
 
 
 export default function Home () {
     return (
-        <Heading size="sm">s</Heading>
-
+        <Grid
+            as="main"
+            height="100vh"
+            templateColluns="1fr 795px 275px 1fr"
+            templateRows="400px 100px 1fr "
+            templateAreas="
+            '. logo decoracao . '
+            '. produto lateral .'            
+            "
+            justfyContent="center"
+            alingItems="center"
+        >
+            <Flex gridArea="logo" flexDir="column" alignItems="flex-start">
+                <img src="/faatlogo.png" alt="logo grupo faat"/>
+            </Flex>
+        </Grid>
 
         )
     }
