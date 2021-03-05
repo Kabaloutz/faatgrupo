efcde5e6ed675084077a1c6f4482e62c337be59c
Kabaloutz <80052137+Kabaloutz@users.noreply.github.com>
2021-03-05 14:39:10 -0400

    h

--- pages/home.tsx
+++ pages/home.tsx
@@ -1,19 +1,21 @@
-import { Heading, Grid, GridItem, Flex, Spacer, Box, Button } from "@chakra-ui/react"
+import {
+    Heading, Grid, GridItem, Flex, Spacer, Box, Button,
+    Link,
+} from "@chakra-ui/react";
 import theme from '../styles/theme';
-
 
 export default function Home() {
     return (
         <Grid
             as="main"
             height="100vh"
-            templateColluns="1fr 1fr 1fr"
-            templateRows="200px 50px 1fr "
+            templateRows="200px 50px 1fr"
             templateAreas="
-            '. cabecalho .'
+            'direita cabecalho esquerda'
             'direita menu esquerda'   
             'direita principal esquerda'          
             "
+            templateCollumns="1fr 480px 80px1fr"
             alingItems="center"
             justfyContent="center"
         >
@@ -21,7 +23,7 @@ export default function Home() {
                 <Box w="100%" h="100%" bg="brand.100">  </Box>
             </Flex>
             {/* Cabeçalho */}
-            <Flex gridArea="cabecalho"  bg="brand.100" center>
+            <Flex gridArea="cabecalho" bg="brand.100" >
                 <Box p="20"><img src="/faatlogo.png" alt="logo do grupo faat" /> </Box>
                 <Spacer />
                 <Box p="20"><img src="/slogan.png" alt="Nosso Slogan: A escolha certa para sua obra" /></Box>
@@ -29,18 +31,11 @@ export default function Home() {
                 <Box p="10"><img src="/mascote.png" alt="Tijolito mascote do grupo" /></Box>
             </Flex>
             <Flex gridArea="menu">
-                <Box bg="yellow.700" w="100%">espaço para menu</Box>
-            </Flex>
-            <Flex gridArea="principal">
-                <Box bg="yellow.900" w="100%">espaço produtos</Box>
-                <Box bg="yellow.700" >
-                    espaço barra lateral
-                    <Box p="3" bg="yellow.900" >banner</Box>
-                    <Spacer />
-                    <Box p="3"bg="yellow.700" >banner</Box>
-                    <Spacer />
-                    <Box p="3"bg="yellow.900" >banner</Box>
-                </Box>
+                <Link>HOME</Link>
+                <Link>EMPRESA</Link>
+                <Link>PRODUTOS</Link>
+                <Link>SIMULADOR</Link>
+                <Link>CONTATOS</Link>
             </Flex>
             <Flex gridArea="esquerda">
                 <Box w="100%" h="100%" bg="brand.100">f </Box>
