7ef5a42e6c40d0a33ece1adb24fc22012c3735ac
Kabaloutz <80052137+Kabaloutz@users.noreply.github.com>
2021-03-05 11:59:38 -0400

    gridv1

--- pages/home.tsx
+++ pages/home.tsx
@@ -18,21 +18,32 @@ export default function Home() {
             justfyContent="center"
         >
             <Flex gridArea="direita">
-                <Box w="100%" h="100%" bg="brand.900">  </Box>
+                <Box w="100%" h="100%" bg="brand.100">  </Box>
             </Flex>
             {/* Cabeçalho */}
-            <Flex gridArea="cabecalho"  bg="yellow.700" center>
-                <Box p="20" bg="brand.900" ><img src="/faatlogo.png" alt="logo do grupo faat" /> </Box>
+            <Flex gridArea="cabecalho"  bg="brand.100" center>
+                <Box p="20"><img src="/faatlogo.png" alt="logo do grupo faat" /> </Box>
                 <Spacer />
                 <Box p="20"><img src="/slogan.png" alt="Nosso Slogan: A escolha certa para sua obra" /></Box>
                 <Spacer />
-                <Box p="10" bg="brand.900"><img src="/mascote.png" alt="Tijolito mascote do grupo" /></Box>
+                <Box p="10"><img src="/mascote.png" alt="Tijolito mascote do grupo" /></Box>
             </Flex>
             <Flex gridArea="menu">
-                <Box bg="yellow.700" w="100%"> f</Box>
+                <Box bg="yellow.700" w="100%">espaço para menu</Box>
+            </Flex>
+            <Flex gridArea="principal">
+                <Box bg="yellow.900" w="100%">espaço produtos</Box>
+                <Box bg="yellow.700" >
+                    espaço barra lateral
+                    <Box p="3" bg="yellow.900" >banner</Box>
+                    <Spacer />
+                    <Box p="3"bg="yellow.700" >banner</Box>
+                    <Spacer />
+                    <Box p="3"bg="yellow.900" >banner</Box>
+                </Box>
             </Flex>
             <Flex gridArea="esquerda">
-                <Box w="100%" h="100%" bg="brand.900">f </Box>
+                <Box w="100%" h="100%" bg="brand.100">f </Box>
             </Flex>
         </Grid>
 
