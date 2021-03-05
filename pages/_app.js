0a70ab74eefe20247fffcb9d51a01effc4610c61
Kabaloutz <80052137+Kabaloutz@users.noreply.github.com>
2021-03-05 10:43:46 -0400

    cabecalho v0

--- pages/_app.js
+++ pages/_app.js
@@ -1,35 +1,13 @@
-import React from 'react';
-import Head from 'next/head';
-import { Global, css } from '@emotion/core';
-import { ThemeProvider, CSSReset } from '@chakra-ui/core';
+import { ChakraProvider, extendTheme } from "@chakra-ui/react"
+import { Global } from "@emotion/react"
+import theme from "../styles/theme"
 
-const MyApp = ({ Component, pageProps }) => {
+const themes = extendTheme(theme)
+export default function App({ Component, pageProps }) {
   return (
-    <ThemeProvider>
-      <Head>
-        <title>Next.js 9.3 + Prisma</title>
-        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
-      </Head>
+    <ChakraProvider theme={themes}>
+      <Global  />
       <Component {...pageProps} />
-      <CSSReset />
-      <Global
-        styles={css`
-          body {
-            background-color: #f7fafc;
-          }
-
-          #__next {
-            display: flex;
-            flex-direction: column;
-            min-height: 100vh;
-            max-width: 600px;
-            margin: 0 auto;
-            padding: 16px;
-          }
-        `}
-      />
-    </ThemeProvider>
-  );
-};
-
-export default MyApp;
+    </ChakraProvider>
+  )
+}
--- pages/home.tsx
+++ pages/home.tsx
@@ -1,27 +1,32 @@
-import {  Heading, Grid, GridItem, Flex, Spacer} from "@chakra-ui/react"
+import { Heading, Grid, GridItem, Flex, Spacer, Box, Button } from "@chakra-ui/react"
+import theme from '../styles/theme';
 
 
-export default function Home () {
+export default function Home() {
     return (
         <Grid
             as="main"
             height="100vh"
-            templateColluns="1fr 795px 275px 1fr"
-            templateRows="400px 100px 1fr "
+            templateColluns="1fr 500px 500px 1fr"
+            templateRows="200px 100px 1fr "
             templateAreas="
-            '. logo decoracao . '
+            '. logo tijolito  . '
             '. produto lateral .'            
             "
             justfyContent="center"
             alingItems="center"
         >
-            <Flex gridArea="logo" flexDir="column" alignItems="flex-start">
-                <img src="/faatlogo.png" alt="logo grupo faat"/>
+
+            <Flex gridArea="logo" >
+                <Box p="10" bg="brand.100"><img src="/faatlogo.png" alt="logo do grupo faat" /> </Box>
+                <Box p="10" bg="brand.100"><img src="/slogan.png" alt="Nosso Slogan: A escolha certa para sua obra" /></Box>
+            </Flex>
+            <Flex gridArea="tijolito">
+                <Box p="10" bg="brand.900"><img src="/mascote.png" alt="Tijolito mascote do grupo" /></Box>
             </Flex>
         </Grid>
 
-        )
-    }
-    
-    
-    
+    )
+}
+
+
