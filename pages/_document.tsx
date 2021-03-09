083403677ad95125690469349f14f89089b44934
Kabaloutz <criativo@kabalpublicidade.com.br>
2021-03-08 20:50:33 -0400

    casa

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -5,7 +5,12 @@ class MyDocument extends Document<DocumentProps> {
         return (
             <Html>
                 <Head>
-                    <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,400;0,700;0,900;1,400;1,700;1,900&display=swap" rel="stylesheet"/>   
+                    <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,400;0,700;0,900;1,400;1,700;1,900&display=swap" rel="stylesheet" />
+                    <link href="https://code.jquery.com/jquery-1.11.0.min.js" />
+                    <link href="https://code.jquery.com/jquery-migrate-1.2.1.min.js" />
+                    <link href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.7.1/slick.js" />
+                    <link href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.7.1/slick.css" rel="stylesheet" />
+                    <link href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.7.1/slick-theme.css" rel="stylesheet" />
                 </Head>
                 <body>
                     <Main />
