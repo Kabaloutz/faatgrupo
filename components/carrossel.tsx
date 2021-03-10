5ac2bee7f2303bb583cecafa6e2c5083e52d5d24
Kabaloutz <80052137+Kabaloutz@users.noreply.github.com>
2021-03-10 09:44:35 -0400

    a

--- components/carrossel.tsx
+++ components/carrossel.tsx
@@ -1,18 +1,23 @@
-import { Box, Container, Flex, HStack, Text, Image, Heading, IconButton, Icon, Circle } from '@chakra-ui/react';
-import { PhoneIcon, ArrowRightIcon, ArrowLeftIcon } from '@chakra-ui/icons'
+import { Box, Container, Flex, HStack, Text, Image, Heading, IconButton, Fade, ScaleFade, Slide, SlideFade, useDisclosure } from '@chakra-ui/react';
+import { ArrowRightIcon, ArrowLeftIcon } from '@chakra-ui/icons'
 import React from 'react';
 import { useState, useEffect } from 'react';
 
 const Carrossel: React.FC = () => {
+    
+    const [mostrar, setMostrar] = useState(false);
     const [posicoes, setPosicoes] = useState([1, 2, 3, 4]);
     const [atual, setAtual] = useState(1);
+
     const Avancar = () => {
         let p = posicoes;
         p.push(p.shift());
         setPosicoes(p);
         setAtual(posicoes[2]);
+        setMostrar(true);
         return atual;
     }
+
     const Retroceder = () => {
         let p = posicoes;
         let b = p.pop();
@@ -25,24 +30,18 @@ const Carrossel: React.FC = () => {
     useEffect(() => {
         const interval = setInterval(() => {
             Avancar();
-        }, 6000);
+        }, 6000);        
+        setMostrar(false);
         return () => clearInterval(interval);
     }, []);
 
-
-
-    function newFunction(interval: NodeJS.Timeout): void  {
-        return clearInterval(interval);
-    }
-
-
     return (
-        <Flex gridArea="carrossel" >
+        <Flex gridArea="carrossel" w="max">
 
             <HStack spacing="25px" d="flex" color="preto.200" >
 
                 <Box >
-                    <IconButton onMouseMove={newFunction} h="300px" onClick={Avancar}
+                    <IconButton  h="300px" onClick={Avancar}  
                         colorScheme="gray"
                         aria-label="Call Segun"
                         size="lg"
@@ -51,7 +50,9 @@ const Carrossel: React.FC = () => {
                 </Box>
 
                 <Box h="100%" >
-                    <Image h="100%" w="100%" src={atual + ".jpg"} />
+                    <ScaleFade initialScale={0.9} in={mostrar}>
+                        <Image h="100%" w="100%" src={atual + ".jpg"} />
+                    </ScaleFade>
                 </Box>
 
                 <Box >
